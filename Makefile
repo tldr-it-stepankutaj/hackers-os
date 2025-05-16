06037ae5ca9243c5a2c074abcae208f7acd46572
Stepan Kutaj <stepan.kutaj@tldr-it.com>
2025-05-16 15:29:19 +0200

    Update application - sku

--- Makefile
+++
@@ -1,8 +0,0 @@
-build:
-	cargo +nightly build -Z build-std=core --target x86_64-hackers_os.json
-
-image:
-	cargo +nightly bootimage
-
-run:
-	qemu-system-x86_64 -drive format=raw,file=target/x86_64-hackers_os/debug/bootimage-hackers_os.bin
--- build.rs
+++
@@ -1,8 +0,0 @@
-use std::{env, fs, path::PathBuf};
-
-fn main() {
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    fs::copy("linker.ld", out_dir.join("linker.ld")).unwrap();
-    println!("cargo:rustc-link-search={}", out_dir.display());
-    println!("cargo:rustc-link-arg=-Tlinker.ld");
-}
